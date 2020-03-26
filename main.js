906e605a5c9f94a8661c3a784c21e38c1842d9e0
rachele <rachele.terzoli@gmail.com>
2020-03-26 10:58:49 +0100

    esercizio commentato

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 for (var i=1; i<=100 ; i++){
+  // check dei numeri divisibili per 3 e per 5 = divisibili per 15
   if ( (i%15) == 0){
     console.log("FizzBuzz");
   }
+  // check numeri divisibili per 3
   else   if ( (i%3) == 0){
       console.log("Fizz");
     }
+    // check numeri divisibili per 5
 else   if ( (i%5) == 0){
     console.log("Buzz");
   }
+  // stampo i restanti numeri
   else {
     console.log(i);
   }
