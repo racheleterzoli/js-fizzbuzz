3542c289022ea7c093812b7caad6c978badc2fd6
rachele <rachele.terzoli@gmail.com>
2020-03-26 12:40:25 +0100

    stampa a video

--- bonus/main.js
+++ bonus/main.js
@@ -2,17 +2,29 @@ for (var i=1; i<=100 ; i++){
   // check dei numeri divisibili per 3 e per 5 = divisibili per 15
   if ( (i%15) == 0){
     console.log("FizzBuzz");
+  var h1 = document.createElement('h1');
+  h1.appendChild(document.createTextNode ("FizzBuzz"));
+  document.body.appendChild(h1);
   }
   // check numeri divisibili per 3
   else   if ( (i%3) == 0){
       console.log("Fizz");
+      var h1 = document.createElement('h1');
+      h1.appendChild(document.createTextNode ("Fizz"));
+      document.body.appendChild(h1);
     }
     // check numeri divisibili per 5
-else   if ( (i%5) == 0){
+else  if ( (i%5) == 0){
     console.log("Buzz");
+    var h1 = document.createElement('h1');
+    h1.appendChild(document.createTextNode ("Fizz"));
+    document.body.appendChild(h1);
   }
   // stampo i restanti numeri
   else {
     console.log(i);
+    var h1 = document.createElement('h1');
+    h1.appendChild(document.createTextNode(i));
+    document.body.appendChild(h1);
   }
 }
